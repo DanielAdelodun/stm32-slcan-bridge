28f66e8d1b442963ef822961ed890fb1d2cf1889
Daniel Adelodun <daniel_adelodun@hotmail.co.uk>
2020-11-20 01:08:15 +0000

    Delete Websocket-Server-checkpoint.py

--- Super-Simple-Server-Example/.ipynb_checkpoints/Websocket-Server-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import isotp
-import socket
-import websockets
-
-# Create the ISOTP socket
-isotp_socket = isotp.socket()
-isotp_socket.bind("slcan0", isotp.Address(rxid=0x7E8, txid=0x7DF))
-print(s)
